dc8fec880e74ca8bf3120a831d1a2672696babff
Altay Hodoglugil <altay.hodo@gmail.com>
2025-01-06 23:18:09 -0800

    Add structure/skeleton

---
+++ src/components/Board.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export default function Board() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  //fetch pokemon data, useEffect- setPokemonList with relevant data-
+  // manually retrieve 12 unique pokemon?
+  
+
+  //shuffle logic
+
+  //handle card click
+
+  return (
+    <div className="card-grid">
+
+    </div>
+  )
+}
