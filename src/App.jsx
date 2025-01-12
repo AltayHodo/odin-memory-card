601a844bf56cd2976a681043bf5df9adece9c479
Altay Hodoglugil <altay.hodo@gmail.com>
2025-01-12 01:54:49 -0800

    Add message under header

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <>
       <h1>Memory Game</h1>
+      <h2>Get points by clicking on an image but don't click on any more than once!
+      </h2>
       <Board />
     </>
   )
